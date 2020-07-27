23f75df0c9d21d52ac17b606369240b3285471ee
Matan Kochavi <kochavimatan@gmail.com>
2020-07-27 11:08:25 +0300

    ``

--- transpool/src/servlets/MapServlet.java
+++ transpool/src/servlets/MapServlet.java
@@ -9,14 +9,20 @@ import utils.ServletUtils;
 import wrappers.MapDataWrapper;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.Scanner;
 
 @WebServlet(name = "MapServlet", urlPatterns = { "/map"})
+@MultipartConfig(fileSizeThreshold = 1024 * 1024, maxFileSize = 1024 * 1024 * 5, maxRequestSize = 1024 * 1024 * 5 * 5)
 public class MapServlet extends HttpServlet {
 
     @Override
@@ -32,16 +38,27 @@ public class MapServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        EngineHandler engineHandler =  ServletUtils.getEngineHandler(req.getServletContext());
-        //TODO: MATAN please handle the load here
         resp.setContentType("application/json;charset=UTF-8");
+
+        //TODO: MATAN please handle the load here
         String response;
+
         try {
-            int logicId = engineHandler.createNewLogicFromXml("/tmp/java/ex1-real.xml");
+
+            EngineHandler engineHandler =  ServletUtils.getEngineHandler(req.getServletContext());
+            Collection<Part> parts = req.getParts();
+            StringBuilder fileContent = new StringBuilder();
+
+            for (Part part : parts) {
+                fileContent.append(readFromInputStream(part.getInputStream()));
+            }
+
+            int logicId = engineHandler.createNewLogicFromXml("c:/temp/ex1-small.xml");
+
             LogicHandler logicHandler = engineHandler.getLogicHandlerById(logicId);
             Gson gson = new Gson();
             MapDataWrapper wrapper = new MapDataWrapper(logicHandler);
-            response = gson.toJson(wrapper);
+            response = gson.toJson(logicId);
 
             //RETURN to client object with {Map, NumOfTremps, NumOfRides}
         } catch (FaildLoadingXMLFileException e) {
@@ -51,6 +68,10 @@ public class MapServlet extends HttpServlet {
         try (PrintWriter out = resp.getWriter()) {
             out.print(response);
         }
+    }
+
+    private String readFromInputStream(InputStream inputStream) {
+        return new Scanner(inputStream).useDelimiter("\\Z").next();
     }
 
 }
