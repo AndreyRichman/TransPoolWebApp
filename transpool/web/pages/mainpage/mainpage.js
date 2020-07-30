75fbecaa930ce699a0f1a3fad37bc3da9598bc3b
Matan Kochavi <kochavimatan@gmail.com>
2020-07-31 02:36:35 +0300

    added online-users. still need to add css

--- transpool/web/pages/mainpage/mainpage.js
+++ transpool/web/pages/mainpage/mainpage.js
@@ -5,10 +5,8 @@ $(function () {
         type: 'GET',
         url: '/transpool_war_exploded/UsersList',
         success: function (data) {
-            // $users.append('<li>name:'+ 'test' + '</li>' );
             $.each(data, function (i, item) {
-                $users.append('<li>'+ item.name +'</li>' )
-
+                $users.append('<li><a href="#">' + item.name + '</a></li>' )
             })
         }
     });
