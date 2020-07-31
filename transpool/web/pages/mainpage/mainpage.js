2d766b858ad5e71ac17bbf1e1fac07a69c9c4829
Matan Kochavi <kochavimatan@gmail.com>
2020-07-31 03:53:05 +0300

    added ONLINE to the servlet. need to remove users from DB when they offline

--- transpool/web/pages/mainpage/mainpage.js
+++ transpool/web/pages/mainpage/mainpage.js
@@ -1,15 +1,43 @@
-$(function () {
-    var $users = $('#online-users');
+var $users = $('#online-users');
+var refreshRate = 2000;
+// $(function () {
+//     $.ajax({
+//         type: 'GET',
+//         url: '/transpool_war_exploded/UsersList',
+//         success: function (data) {
+//             $.each(data, function (i, username) {
+//                 $users.append('<li><a href="#">' + username.name + '</a></li>' )
+//             })
+//         }
+//     });
+// });
 
+function ajaxUsersList() {
     $.ajax({
-        type: 'GET',
         url: '/transpool_war_exploded/UsersList',
-        success: function (data) {
-            $.each(data, function (i, item) {
-                $users.append('<li><a href="#">' + item.name + '</a></li>' )
-            })
+        success: function(users) {
+            refreshUsersList(users);
         }
     });
+}
+
+function refreshUsersList(users) {
+    //clear all current users
+    $users.empty();
+
+    // rebuild the list of users: scan all users and add them to the list of users
+    $.each(users || [], function(index, username) {
+        console.log("Adding user #" + index + ": " + username);
+        //create a new <option> tag with a value in it and
+        //appeand it to the #userslist (div with id=userslist) element
+        $users.append('<li><a href="#">' + username.name + '</a></li>' )
+    });
+}
+
+$(function() {
+
+    //The users list is refreshed automatically every second
+    setInterval(ajaxUsersList, refreshRate);
 });
 
 //TODO:: need to add ONLINE users. right now its just showing who's in RIGHT NOW. need to refresh uppon changes
